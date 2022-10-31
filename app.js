bc2154b984d8c77bdf3d22f1c18f1fe2ebacb915
noelia <sacacanoelia@gmail.com>
2022-10-31 00:06:26 -0300

    desafio 2: Manejo de Archivos en Javascript

--- app.js
+++ app.js
@@ -1,42 +1,67 @@
-class Usuario {
-    constructor(nombre, apellido, mascotas, libros) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.mascotas = [mascotas];
-        this.libros = [libros];
+const fs = require('fs')
+class Contenedor {
+    constructor(nameArchivo) {
+        this.nameArc = nameArchivo
     }
 
-    getFullName() {
-        return console.log(`Nombre completo: ${this.nombre} ${this.apellido}`);
+    async save(object) {
+        let auxArray = []
+        try {
+            const data = await fs.promises.readFile(this.nameArc, "utf-8")
+            auxArray = JSON.parse(data)
+            let idArray = auxArray.map(obj => obj.id)
+            let highId = Math.max(...idArray)
+            object.id = highId + 1;
+            auxArray.push(object);
+            fs.writeFileSync(this.nameArc, JSON.stringify(auxArray))
+        }
+        catch {
+            object.id = 0;
+            auxArray.push(object);
+            fs.writeFileSync(this.nameArc, JSON.stringify(auxArray))
+        }
+        return object.id
     }
-
-    addMascota(nuevaMascota) {
-        this.mascotas.push(nuevaMascota);
+    async getById(number) {
+        try {
+            const data = await fs.promises.readFile(this.nameArc, "utf-8")
+            let auxArray = JSON.parse(data)
+            const object = auxArray.find(obj => obj.id === number)
+            return object
+        }
+        catch {
+            return null
+        }
     }
-
-    countMascota() {
-        return console.log(`Mascotas: ${this.mascotas.length}`);
+    async getAll() {
+        try {
+            const data = await fs.promises.readFile(this.nameArc, "utf-8")
+            const auxArray = JSON.parse(data)
+            return auxArray
+        }
+        catch {
+            return null
+        }
     }
-
-    addBook(nombre, autor) {
-        this.libros.push({ nombre: nombre, autor: autor });
+    async deleteById(number) {
+        try {
+            const data = await fs.promises.readFile(this.nameArc, "utf-8")
+            const auxArray = JSON.parse(data)
+            const newArray = auxArray.filter(obj => obj.id !== number)
+            fs.writeFileSync(this.nameArc, JSON.stringify(newArray))
+        }
+        catch {
+            return "No hay objetos en el archivo"
+        }
     }
-    getBookNames() {
-        const nombreLibro = [];
-        this.libros.map((libro) => nombreLibro.push(libro.nombre));
-        return console.log(`Libros: ${nombreLibro}`);
+    deleteAll() {
+        fs.writeFileSync(this.nameArc, "")
     }
 }
 
-let usuario = new Usuario("Tomas","Carlos",["Loro", "Perro"],[
-        { nombre: "El libro de la selva", autor: "Rudyard Kipling" },
-        { nombre: "El principito", autor: "Antoine de Saint-Exupéry" },
-    ]
-);
-
-usuario.getFullName();
-usuario.addMascota("Gato");
-console.log(usuario.mascotas);
-usuario.countMascota();
-usuario.addBook("Casa Tomada", "Julio Cortazar");
-usuario.getBookNames();
+const newArchivo = new Contenedor("./archivos.txt");
+newArchivo.save({ title: "taza con diseño de sandia", price: 1500, thumbnail: "tazaPorcelana.png" }).then(resolve => console.log(resolve));
+newArchivo.getById(1).then(resolve => console.log(resolve));
+newArchivo.getAll().then(resolve => console.log(resolve));
+newArchivo.deleteById(2);
+newArchivo.deleteAll();
